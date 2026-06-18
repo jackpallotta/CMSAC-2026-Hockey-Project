5ea37005912ab454b263de38f1af09f4489aea06
Chloe Guagliano <chloeguagliano@gmail.com>
2026-06-18 15:01:00 -0400

    :)

--- Chloe Starter Script.R
+++ Chloe Starter Script.R
@@ -1,1 +1,13 @@
 ## Chloe Starter Script
+
+library(nhlscraper)
+
+nhl_rink = draw_NHL_rink()
+
+pbp = gc_play_by_play(game = 2023030417)
+
+shiftchart = shift_chart(game = 2023030417)
+
+player_summary(player = 8478402)$shootsCatches
+
+gc_summary(game = 2023030417)

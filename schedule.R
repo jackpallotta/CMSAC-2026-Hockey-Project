4b8cfc97b13116df0c599b67279be475a6f2bde6
Jack Pallotta <jackpallotta@Jacks-MacBook-Air.local>
2026-06-22 13:44:52 -0400

    Scrape the NHL schedule

---
+++ schedule.R
@@ -0,0 +1,42 @@
+rm(list=ls())
+library(tidyverse)
+library(httr)
+library(jsonlite)
+library(glue)
+library(purrr)
+
+seasons <- c(20212022, 20222023, 20232024, 20242025, 20252026)
+
+get_schedule_api <- function(team_id, season_id) {
+  "https://api-web.nhle.com/v1/club-schedule-season/{team_id}/{season_id}" |>
+    glue() |>
+    GET() |>
+    content(type = "text", encoding = "UTF-8") |>
+    fromJSON(flatten = TRUE) -> response
+  
+  response$games
+  
+}
+
+nhl_teams <- c("ANA", "ARI", "BOS", "BUF", "CAR", "CBJ", "CGY", "CHI", "COL", "DAL", 
+               "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NJD", "NSH", "NYI", 
+               "NYR", "OTT", "PHI", "PIT", "SEA", "SJS", "STL", "TBL", "TOR", 
+               "UTA", "VAN", "VGK", "WSH", "WPG")
+
+schedule <- seasons |>
+  map_dfr(\(season) {
+    nhl_teams |>
+      map_dfr(\(team) {
+        get_schedule_api(team, season)
+      })
+  }) |>
+  filter(gameType == 2) |>
+  rename(gameId = id, awayTeamCode = awayTeam.abbrev, awayTeamId = awayTeam.id, 
+         homeTeamCode = homeTeam.abbrev, homeTeamId = homeTeam.id) |>
+  distinct(gameId, .keep_all = TRUE) |>
+  arrange(gameId) |>
+  select(gameId, season, gameType, gameDate, awayTeamCode, awayTeamId,
+         homeTeamCode, homeTeamId)
+
+saveRDS(schedule, "schedule.rds")
+schedule <- readRDS("/Users/jackpallotta/Desktop/schedule.rds")

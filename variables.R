9d099923b42e54f8dbd5ed1a594f1300108d1046
Jack Pallotta <jackpallotta@Jacks-MacBook-Air.local>
2026-07-10 12:58:39 -0400

    Variables script.

--- variables.R
+++ variables.R
@@ -7,7 +7,7 @@ rosters <- readRDS("rosters.rds")
 players <- readRDS("players.rds")
 careerGameNumbersCleaned <- readRDS("careerGameNumbersCleaned.rds")
 
-pbp_cleaned <- pbp_cleaned |>
+pbp_cleaned_temp <- pbp_cleaned |>
   mutate(coordinates = paste(xCoord, yCoord, sep = ", ")) |>
   select(gameId, eventId, seasonId, periodNumber, periodType, secondsElapsedInGame, eventOwnerTeamId,
          eventTeamVenue, eventTypeDescKey, situationCode, isHome, homeIsEmptyNet, awayIsEmptyNet, 
@@ -16,14 +16,14 @@ pbp_cleaned <- pbp_cleaned |>
          zoneCode, coordinates, winningPlayerId, losingPlayerId)
 
 # faceoff-winning team's perspective
-faceoffs <- pbp_cleaned |>
+faceoffs_temp <- pbp_cleaned_temp |>
   filter(eventTypeDescKey == "faceoff") |>
   left_join(schedule |> select(gameId, gameDate, awayTeamId, homeTeamId, winningTeamId), by = "gameId") |>
   mutate(faceoffWon = 1L,
          wonGame = as.integer(eventOwnerTeamId == winningTeamId))
 
 # mirror every faceoff from the losing team's perspective
-mirrored_faceoffs <- faceoffs |>
+mirrored_faceoffs <- faceoffs_temp |>
   mutate(eventOwnerTeamId = if_else(
     eventOwnerTeamId == homeTeamId,
     awayTeamId,
@@ -50,10 +50,10 @@ mirrored_faceoffs <- faceoffs |>
   select(-temp)
 
 # combine the data
-combined_faceoffs <- bind_rows(faceoffs, mirrored_faceoffs)
+faceoffs <- bind_rows(faceoffs_temp, mirrored_faceoffs)
 
 # cap the goal differential at +/- 3
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   arrange(gameId, eventId, periodNumber, secondsElapsedInGame) |>
   mutate(goalDifferential = case_when(
       goalDifferential <= -3 ~ -3L,
@@ -61,18 +61,18 @@ combined_faceoffs <- combined_faceoffs |>
       TRUE ~ as.integer(goalDifferential)))
 
 # create a variable for the faceoff player, each unique faceoff has two observations
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   mutate(faceoffPlayerId = case_when(
     faceoffWon == 1 ~ winningPlayerId,
     faceoffWon == 0 ~ losingPlayerId))
 
 # join the players df to get the handedness of the two faceoff players
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   left_join(players |> select(season, playerId, player, shoots, birthDate),
             by = c("faceoffPlayerId" = "playerId", "seasonId" = "season"))
 
 # create a variable for whether a player takes the faceoff on his strong side
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   mutate(strongSide = case_when(
     eventTeamVenue == "home" & homeTeamDefendingSide == "left" & shoots == "L" &
       coordinates %in% c("-69, 22", "-20, 22", "20, 22", "69, 22") ~ 1,
@@ -95,11 +95,10 @@ combined_faceoffs <- combined_faceoffs |>
       coordinates %in% c("-69, -22", "-20, -22", "20, -22", "69, -22") ~ 1,
    
     coordinates %in% c("0, 0") ~ 0,
-    TRUE ~ 0
-  ))
+    TRUE ~ 0))
 
 # create a variable for which player places his stick down first
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   mutate(stickDownFirst = case_when(
     eventTeamVenue == "home" & homeTeamDefendingSide == "left" &
         coordinates %in% c("-69, 22", "-20, 22", "-69, -22", "-20, -22") ~ 1,
@@ -113,121 +112,165 @@ combined_faceoffs <- combined_faceoffs |>
       TRUE ~ 0))
 
 # create a variable for the player age
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   mutate(age = interval(birthDate, gameDate) %/% years(1)) |>
   select(-birthDate)
 
 # add career game experience
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   left_join(careerGameNumbersCleaned |> select(playerId, gameId, seasonGameNumber, careerGameNumber),
             by = c("faceoffPlayerId" = "playerId", "gameId" = "gameId"))
 
 # add faceoff number taken in game
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   group_by(gameId, faceoffPlayerId) |>
   arrange(secondsElapsedInGame, .by_group = TRUE) |>
   mutate(faceoffGameCount = row_number()) |>
   ungroup()
 
 # add faceoff number taken in season
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   group_by(seasonId, faceoffPlayerId) |>
   arrange(gameDate, gameId, secondsElapsedInGame, .by_group = TRUE) |>
   mutate(seasonFaceoffCount = row_number()) |>
   ungroup()
 
 # calculate time remaining in mins and secs for reg & OT
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   mutate(secondsRemainingReg = pmax(3600 - secondsElapsedInGame, 0),
     minutesRemainingReg = secondsRemainingReg / 60,
     secondsRemainingOT = if_else(periodNumber == 4,
       pmax(3900 - secondsElapsedInGame, 0), NA_real_),
     minutesRemainingOT = secondsRemainingOT / 60)
 
-combined_faceoffs <- combined_faceoffs |>
+faceoffs <- faceoffs |>
   arrange(seasonId, gameDate, gameId, eventId)
 
-faceoffsCleaned <- combined_faceoffs |>
+# establish unblocked shot attempt events
+unblocked_events <- c("goal", "shot-on-goal", "missed-shot")
+
+# create faceoff windows for within 5 seconds
+faceoff_windows <- faceoffs |>
+  mutate(faceoffId = row_number()) |>
+  select(faceoffId, gameId, periodNumber, faceoffTeamId = eventOwnerTeamId,
+         faceoffTime = secondsElapsedInGame) |>
+  mutate(faceoffEnd = faceoffTime + 5)
+
+# inner join pbp events
+events_after_faceoff <- faceoff_windows |>
+  inner_join(pbp_cleaned |> filter(eventTypeDescKey %in% unblocked_events) |>
+      select(gameId, periodNumber, secondsElapsedInGame, eventOwnerTeamId, eventTypeDescKey, xG),
+    by = join_by(gameId, periodNumber, 
+                 faceoffTime < secondsElapsedInGame, 
+                 faceoffEnd >= secondsElapsedInGame)) |>
+  mutate(isFor = eventOwnerTeamId == faceoffTeamId)
+
+# summarise the USAT for/against within 5 seconds of each faceoff event
+faceoff_summary <- events_after_faceoff |>
+  group_by(faceoffId) |>
+  summarise(USATFor5 = as.integer(any(isFor)),
+    USATAgainst5 = as.integer(any(!isFor)),
+    USATCountFor5 = sum(isFor),
+    USATCountAgainst5 = sum(!isFor),
+    xGFor5 = sum(if_else(isFor, xG, 0), na.rm = TRUE),
+    xGAgainst5 = sum(if_else(!isFor, xG, 0), na.rm = TRUE),
+    .groups = "drop")
+
+# join the faceoff summary with other faceoff variables
+faceoffs <- faceoffs |>
+  mutate(faceoffId = row_number()) |>
+  left_join(faceoff_summary, by = "faceoffId") |>
+  mutate(across(c(USATFor5,
+                  USATAgainst5,
+                  USATCountFor5,
+                  USATCountAgainst5,
+                  xGFor5,
+                  xGAgainst5), ~ replace_na(.x, 0))) |>
+  select(-faceoffId)
+
+# create faceoff dot category variables
+faceoffs <- faceoffs |>
+  mutate(faceoffDotCategory = recode(
+    coordinates, "-69, 22" = "1",
+    "-20, 22" = "2",
+    "20, 22" = "3",
+    "69, 22" = "4",
+    "-69, -22" = "5",
+    "-20, -22" = "6",
+    "20, -22" = "7",
+    "69, -22" = "8",
+    "0, 0" = "9",
+    .default = NA_character_))
+
+# create faceoff situation from faceoff player's perspective
+faceoffs <- faceoffs |>
+  mutate(teamDefendingSide = case_when(
+      eventTeamVenue == "home" ~ homeTeamDefendingSide,
+      eventTeamVenue == "away" & homeTeamDefendingSide == "left"  ~ "right",
+      eventTeamVenue == "away" & homeTeamDefendingSide == "right" ~ "left",
+      TRUE ~ NA_character_),
+    
+    # determine whether dot is on the left or right
+    faceoffSide = case_when(
+      coordinates == "0, 0" ~ "C",
+      
+      teamDefendingSide == "right" &
+        coordinates %in% c("-69, 22", "-20, 22", "20, 22", "69, 22") ~ "R",
+      
+      teamDefendingSide == "right" &
+        coordinates %in% c("-69, -22", "-20, -22", "20, -22", "69, -22") ~ "L",
+      
+      teamDefendingSide == "left" &
+        coordinates %in% c("-69, 22", "-20, 22", "20, 22", "69, 22") ~ "L",
+      
+      teamDefendingSide == "left" &
+        coordinates %in% c("-69, -22", "-20, -22", "20, -22", "69, -22") ~ "R",
+      
+      TRUE ~ NA_character_
+    ),
+    
+    # classify faceoff situation
+    faceoffSituation = case_when(
+      coordinates == "0, 0" ~ "N-C",
+      
+      # OZ faceoffs
+      zoneCode == "O" & faceoffSide == "R" ~ "O-R",
+      zoneCode == "O" & faceoffSide == "L" ~ "O-L",
+      
+      # DZ faceoffs
+      zoneCode == "D" & faceoffSide == "R" ~ "D-R",
+      zoneCode == "D" & faceoffSide == "L" ~ "D-L",
+      
+      # NZ defensive side
+      zoneCode == "N" & teamDefendingSide == "right" &
+        coordinates %in% c("20, 22", "20, -22") ~ paste0("N-D-", faceoffSide),
+      
+      zoneCode == "N" & teamDefendingSide == "left" &
+        coordinates %in% c("-20, 22", "-20, -22") ~ paste0("N-D-", faceoffSide),
+      
+      # NZ offensive side
+      zoneCode == "N" & teamDefendingSide == "right" &
+        coordinates %in% c("-20, 22", "-20, -22") ~ paste0("N-O-", faceoffSide),
+      
+      zoneCode == "N" & teamDefendingSide == "left" &
+        coordinates %in% c("20, 22", "20, -22") ~ paste0("N-O-", faceoffSide),
+      
+      TRUE ~ NA_character_))
+
+# clean the dataset and select desired varaibles for modeling
+faceoffsCleaned <- faceoffs |>
   select(seasonId, gameId, gameDate, eventId, periodNumber, periodType,
          minutesRemainingReg, secondsRemainingReg, minutesRemainingOT,
-         secondsRemainingOT, eventTypeDescKey, eventTeamVenue, homeTeamDefendingSide,
+         secondsRemainingOT, eventTypeDescKey, eventTeamVenue, teamDefendingSide,
          strengthState, isEmptyNetFor, isEmptyNetAgainst, skaterCountFor, skaterCountAgainst,
-         manDifferential, goalDifferential, zoneCode, coordinates, faceoffPlayerId,
-         player, shoots, strongSide, stickDownFirst, age, faceoffGameCount,
-         seasonFaceoffCount, seasonGameNumber, careerGameNumber, faceoffWon, wonGame)
+         manDifferential, goalDifferential, zoneCode, coordinates, faceoffDotCategory, 
+         faceoffSituation, faceoffPlayerId, player, shoots, strongSide, stickDownFirst, age, 
+         faceoffGameCount, seasonFaceoffCount, seasonGameNumber, careerGameNumber, faceoffWon,
+         USATFor5, USATAgainst5, USATCountFor5, USATCountAgainst5, xGFor5, xGAgainst5,
+         wonGame)
 
-test_game <- combined_faceoffs |>
+test_game <- faceoffsCleaned |>
   filter(gameId == 2025021312) |>
   arrange(eventId)
 
-#Play by play Official----
-#pbp pulled from Jack
-#pulls the first five seconds after a face-off
-pbp_faceoffs = pbp_cleaned |>
-  mutate(row_id = row_number()) |>
-  filter(eventTypeDescKey == "faceoff") |>
-  select(faceoff_row = row_id,
-         gameId,                 
-         periodNumber,                 
-         faceoff_time = secondsElapsedInGame) |>
-  mutate(faceoff_end = faceoff_time + 5)
-
-events_after_faceoff2 = pbp_faceoffs |>
-  inner_join(
-    pbp,
-    by = join_by(
-      gameId == gameId,
-      periodNumber == periodNumber,
-      faceoff_time < secondsElapsedInGame,   
-      faceoff_end >= secondsElapsedInGame)) |>
-  mutate(fo_success = as.factor(ifelse(eventTypeDescKey == "shot-on-goal" | eventTypeDescKey =="goal", 1, 0))) |>
-  mutate(is_shot_atmpt = as.numeric(eventTypeDescKey == "shot-on-goal" | eventTypeDescKey == "missed-shot" | eventTypeDescKey == "goal")) |>
-  mutate(faceoffDotCategory = case_when(xCoord == -69 & yCoord == 22 ~ '1',
-                                        xCoord == -20 & yCoord == 22 ~ '2',
-                                        xCoord == 20 & yCoord == 22 ~ '3',
-                                        xCoord == 69 & yCoord == 22 ~ '4',
-                                        xCoord == -69 & yCoord == -22 ~ '5',
-                                        xCoord == -20 & yCoord == -22 ~ '6',
-                                        xCoord == 20 & yCoord == -22 ~ '7',
-                                        xCoord == 69 & yCoord == -22 ~ '8',
-                                        xCoord == 0 & yCoord == 0 ~ '0')) |>
-  mutate(leftRight = as.factor(case_when(homeTeamDefendingSide == 'left' & eventTeamVenue == 'home'~
-                                           if_else(faceoffDotCategory %in% c('1','2','3','4'),'L','R'),
-                                         homeTeamDefendingSide == 'left' & eventTeamVenue == 'away'~
-                                           if_else(faceoffDotCategory %in% c('5','6','7','8'),'L','R'),
-                                         homeTeamDefendingSide == 'right' & eventTeamVenue == 'home'~
-                                           if_else(faceoffDotCategory %in% c('5','6','7','8'),'L','R'),
-                                         homeTeamDefendingSide == 'right' & eventTeamVenue == 'away'~
-                                           if_else(faceoffDotCategory %in% c('1','2','3','4'),'L','R')))) |>
-  mutate(zoneCode = as.factor(if_else(zoneCode == 'N' & faceoffDotCategory != 'C',
-                                      case_when(homeTeamDefendingSide == 'left' & eventTeamVenue == 'home' & faceoffDotCategory %in% c('2','6') ~ 
-                                                  'C-NZ',
-                                                homeTeamDefendingSide == 'left' & eventTeamVenue == 'home' & faceoffDotCategory %in% c('3','7') ~
-                                                  'F-NZ',
-                                                homeTeamDefendingSide == 'left' & eventTeamVenue == 'away' & faceoffDotCategory %in% c('2','6') ~
-                                                  'F-NZ',
-                                                homeTeamDefendingSide == 'left' & eventTeamVenue == 'away' & faceoffDotCategory %in% c('3','7') ~
-                                                  'C-NZ',
-                                                homeTeamDefendingSide == 'right' & eventTeamVenue == 'home' & faceoffDotCategory %in% c('2','6') ~
-                                                  'F-NZ',
-                                                homeTeamDefendingSide == 'right' & eventTeamVenue == 'home' & faceoffDotCategory %in% c('3','7') ~
-                                                  'C-NZ',
-                                                homeTeamDefendingSide == 'right' & eventTeamVenue == 'away' & faceoffDotCategory %in% c('2','6') ~
-                                                  'C-NZ',
-                                                homeTeamDefendingSide == 'right' & eventTeamVenue == 'away' & faceoffDotCategory %in% c('3','7') ~
-                                                  'F-NZ'),zoneCode))) |>
-  mutate(zoneCode = if_else(zoneCode == 'N','C', zoneCode))|>
-  mutate(situationDescriptor = as.factor(case_when(is.na(zoneCode) | is.na(leftRight) ~ NA_character_,
-                                                   TRUE ~ paste(zoneCode, leftRight, sep = ' ')))) |>
-  mutate(situationLinkage = as.factor(case_when(situationDescriptor %in% c('D L', 'O R') ~
-                                                  'A',
-                                                situationDescriptor %in% c('D R', 'O L') ~
-                                                  'B',
-                                                situationDescriptor %in% c('F-NZ R', 'C-NZ L') ~
-                                                  'C',
-                                                situationDescriptor %in% c('F-NZ L', 'C-NZ R') ~
-                                                  'D',
-                                                situationDescriptor == 'C R' ~
-                                                  'E'))) |>
-  mutate(periodNumber = as.factor(periodNumber))
-
+saveRDS(faceoffsCleaned, "faceoffsCleaned.rds")

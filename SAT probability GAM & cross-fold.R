be634541fac74198c0383c95c1583bf65b78c00e
Jack Pallotta <jackpallotta@Jacks-MacBook-Air.local>
2026-07-12 22:10:19 -0400

    SAT probability model.

--- SAT probability GAM & cross-fold.R
+++ SAT probability GAM & cross-fold.R
@@ -5,9 +5,7 @@ library(mgcv)
 library(pROC)
 
 leverageVariables <- leverageVariables |>
-  mutate(
-    manDifferential = factor(manDifferential)
-  )
+  mutate(manDifferential = factor(manDifferential))
 
 set.seed(711)
 
@@ -16,11 +14,7 @@ N_FOLDS <- 5
 # assign entire games to folds
 game_folds <- leverageVariables |>
   distinct(gameId) |>
-  mutate(
-    fold = sample(
-      rep(1:N_FOLDS, length.out = n())
-    )
-  )
+  mutate(fold = sample(rep(1:N_FOLDS, length.out = n())))
 
 leverageVariables <- leverageVariables |>
   select(-any_of("fold")) |>
@@ -40,151 +34,55 @@ sat_cv <- function(x) {
   sat_test <- leverageVariables |>
     filter(fold == x)
   
-  gam_fit <- bam(
-    SATFor5 ~
-      goalDifferential +
-      s(secondsRemaining, by = goalDifferential, k = 10) +
-      zoneCode * manDifferential +
-      isOT +
-      isEmptyNetFor +
-      isEmptyNetAgainst +
-      logAvailableSATWindow,
-    family = binomial(),
-    method = "fREML",
-    discrete = TRUE,
-    data = sat_train
-  )
+  gam_fit <- bam(SATFor5 ~ goalDifferential + s(secondsRemaining, by = goalDifferential, k = 10) +
+      zoneCode * manDifferential + isOT + isEmptyNetFor + isEmptyNetAgainst + logAvailableSATWindow,
+    family = binomial, method = "fREML", discrete = TRUE, data = sat_train)
   
-  logit_fit <- glm(
-    SATFor5 ~
-      goalDifferential * ns(secondsRemaining, df = 7) +
-      zoneCode * manDifferential +
-      isOT +
-      isEmptyNetFor +
-      isEmptyNetAgainst +
-      logAvailableSATWindow,
-    family = binomial(),
-    data = sat_train
-  )
+  logit_fit <- glm(SATFor5 ~ goalDifferential * ns(secondsRemaining, df = 7) +
+      zoneCode * manDifferential + isOT + isEmptyNetFor + isEmptyNetAgainst +
+      logAvailableSATWindow, family = binomial, data = sat_train)
   
-  gam_pred <- predict(
-    gam_fit,
-    newdata = sat_test,
-    type = "response"
-  )
+  gam_pred <- predict(gam_fit, newdata = sat_test, type = "response")
   
-  logit_pred <- predict(
-    logit_fit,
-    newdata = sat_test,
-    type = "response"
-  )
+  logit_pred <- predict(logit_fit, newdata = sat_test, type = "response")
   
-  stopifnot(
-    !anyNA(gam_pred),
-    !anyNA(logit_pred)
-  )
+  stopifnot(!anyNA(gam_pred), !anyNA(logit_pred))
   
-  tibble(
-    gam_pred = gam_pred,
+  tibble(gam_pred = gam_pred,
     logit_pred = logit_pred,
     test_actual = sat_test$SATFor5,
-    test_fold = x
-  )
+    test_fold = x)
 }
 
-sat_preds <- map(
-  1:N_FOLDS,
-  sat_cv
-) |>
+sat_preds <- map(1:N_FOLDS, sat_cv) |>
   list_rbind()
 
 sat_cv_by_fold <- sat_preds |>
   group_by(test_fold) |>
-  summarise(
-    gam_auc = as.numeric(
-      auc(
-        roc(
-          response = test_actual,
-          predictor = gam_pred,
-          quiet = TRUE
-        )
-      )
-    ),
-    
-    logit_auc = as.numeric(
-      auc(
-        roc(
-          response = test_actual,
-          predictor = logit_pred,
-          quiet = TRUE
-        )
-      )
-    ),
-    
-    gam_brier = mean(
-      (test_actual - gam_pred)^2,
-      na.rm = TRUE
-    ),
-    
-    logit_brier = mean(
-      (test_actual - logit_pred)^2,
-      na.rm = TRUE
-    ),
-    
-    .groups = "drop"
-  )
+  summarize(gam_auc = as.numeric(auc(roc(response = test_actual, predictor = gam_pred, quiet = TRUE))),
+    logit_auc = as.numeric(auc(roc(response = test_actual, predictor = logit_pred, quiet = TRUE))),
+    gam_brier = mean((test_actual - gam_pred)^2, na.rm = TRUE),
+    logit_brier = mean((test_actual - logit_pred)^2, na.rm = TRUE),
+    .groups = "drop")
 
 sat_cv_by_fold
 
 sat_cv_summary <- sat_cv_by_fold |>
-  summarise(
-    mean_gam_auc = mean(gam_auc),
+  summarize(mean_gam_auc = mean(gam_auc),
     mean_logit_auc = mean(logit_auc),
-    
     mean_gam_brier = mean(gam_brier),
     mean_logit_brier = mean(logit_brier),
-    
     sd_gam_auc = sd(gam_auc),
     sd_logit_auc = sd(logit_auc),
-    
     sd_gam_brier = sd(gam_brier),
-    sd_logit_brier = sd(logit_brier)
-  )
+    sd_logit_brier = sd(logit_brier))
 
 sat_cv_summary
 
 sat_cv_overall <- sat_preds |>
-  summarise(
-    gam_auc = as.numeric(
-      auc(
-        roc(
-          response = test_actual,
-          predictor = gam_pred,
-          quiet = TRUE
-        )
-      )
-    ),
-    
-    logit_auc = as.numeric(
-      auc(
-        roc(
-          response = test_actual,
-          predictor = logit_pred,
-          quiet = TRUE
-        )
-      )
-    ),
-    
-    gam_brier = mean(
-      (test_actual - gam_pred)^2,
-      na.rm = TRUE
-    ),
-    
-    logit_brier = mean(
-      (test_actual - logit_pred)^2,
-      na.rm = TRUE
-    )
-  )
+  summarize(gam_auc = as.numeric(auc(roc(response = test_actual, predictor = gam_pred, quiet = TRUE))),
+            logit_auc = as.numeric(auc(roc(response = test_actual, predictor = logit_pred, quiet = TRUE))),
+            gam_brier = mean((test_actual - gam_pred)^2, na.rm = TRUE),
+            logit_brier = mean((test_actual - logit_pred)^2, na.rm = TRUE))
 
 sat_cv_overall
-

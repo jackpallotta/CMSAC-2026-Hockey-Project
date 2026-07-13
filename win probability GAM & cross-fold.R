ed20204b698ab63079fc855c34bdac15d37bcad1
Jack Pallotta <jackpallotta@Jacks-MacBook-Air.local>
2026-07-12 22:01:25 -0400

    Win probability model.

--- win probability GAM & cross-fold.R
+++ win probability GAM & cross-fold.R
@@ -33,87 +33,27 @@ wp_cv <- function(x) {
   wp_test <- leverageVariables |>
     filter(fold == x)
   
-  gam_fit <- bam(
-    wonGame ~
-      goalDifferential +
-      s(
-        secondsRemaining,
-        by = goalDifferential,
-        k = 10
-      ) +
-      zoneCode * manDifferential +
-      isOT +
-      isEmptyNetFor +
-      isEmptyNetAgainst,
-    family = binomial(link = "logit"),
-    method = "fREML",
-    discrete = TRUE,
-    data = wp_train
-  )
+  gam_fit <- bam(wonGame ~ goalDifferential + s(secondsRemaining, by = goalDifferential, k = 10) +
+      zoneCode * manDifferential + isOT + isEmptyNetFor + isEmptyNetAgainst,
+    family = binomial(link = "logit"), method = "fREML", discrete = TRUE, data = wp_train)
   
-  logit_fit <- glm(
-    wonGame ~
-      goalDifferential * ns(secondsRemaining, df = 7) +
-      zoneCode * manDifferential +
-      isOT +
-      isEmptyNetFor +
-      isEmptyNetAgainst,
-    family = binomial(link = "logit"),
-    data = wp_train
-  )
+  logit_fit <- glm(wonGame ~ goalDifferential * ns(secondsRemaining, df = 7) + 
+                     zoneCode * manDifferential + isOT + isEmptyNetFor + isEmptyNetAgainst,
+                   family = binomial(link = "logit"), data = wp_train)
   
-  tibble(
-    gam_pred = predict(
-      gam_fit,
-      newdata = wp_test,
-      type = "response"
-    ),
-    logit_pred = predict(
-      logit_fit,
-      newdata = wp_test,
-      type = "response"
-    ),
-    test_actual = wp_test$wonGame,
-    test_fold = x
-  )
+  tibble(gam_pred = predict(gam_fit, newdata = wp_test, type = "response"),
+    logit_pred = predict(logit_fit, newdata = wp_test, type = "response"),
+    test_actual = wp_test$wonGame, test_fold = x)
 }
 
 wp_preds <- map(1:N_FOLDS, wp_cv) |> list_rbind()
 
 wp_cv_by_fold <- wp_preds |>
   group_by(test_fold) |>
-  summarise(
-    gam_auc = as.numeric(
-      auc(
-        roc(
-          test_actual,
-          gam_pred,
-          quiet = TRUE
-        )
-      )
-    ),
-    
-    logit_auc = as.numeric(
-      auc(
-        roc(
-          test_actual,
-          logit_pred,
-          quiet = TRUE
-        )
-      )
-    ),
-    
-    gam_brier = mean(
-      (test_actual - gam_pred)^2,
-      na.rm = TRUE
-    ),
-    
-    logit_brier = mean(
-      (test_actual - logit_pred)^2,
-      na.rm = TRUE
-    ),
-    
-    .groups = "drop"
-  )
+  summarize(gam_auc = as.numeric(auc(roc(test_actual, gam_pred, quiet = TRUE))),
+            logit_auc = as.numeric(auc(roc(test_actual, logit_pred, quiet = TRUE))),
+            gam_brier = mean((test_actual - gam_pred)^2,na.rm = TRUE),
+            logit_brier = mean((test_actual - logit_pred)^2, na.rm = TRUE),
+            .groups = "drop")
 
 wp_cv_by_fold
